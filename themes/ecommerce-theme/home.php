50af10a23e81e359edb120e469fadea40cd6d87d
Qian <qian.ma3@dcmail.ca>
2020-03-07 20:13:17 -0500

    blog archive page structure created

--- themes/ecommerce-theme/home.php
+++ themes/ecommerce-theme/home.php
@@ -1,60 +1,76 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package E-commerce_Theme
+/*
+ * Template Name: Custom
  */
 
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main grid-x grid-margin-x">
+			<div class=" cell large-1"></div>
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+			<article class="cell large-10" id="bodyText">
+			<aside >
 				<?php
-			endif;
-            ?>
-            <?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				/**
+				 * Custom WP_Query
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				$args = array(
+					'post_type'      => 'post',
+					'posts_per_page' => 3,
+				);
 
-			endwhile;
+				$test_query = new WP_Query( $args );
 
-			the_posts_navigation();
+				if ( $test_query->have_posts() ) {
+				?>
+					<div>
+						<div class="post-list height-1">
+							<div class="grid-x grid-margin-x grid-margin-y">
+								
+								<?php
+								while ( $test_query->have_posts() ) {
+									$test_query->the_post();
+									?>
+									<div class="cell small-12 medium-6 large-4 custom post-list-wrapper">
+										<div id="post-<?php echo get_the_ID(); ?>">
+											<a href="<?php the_permalink(); ?>"><h4 class="text-center postTitle"><?php echo get_the_title(); ?></h4></a>
+											<!-- post-thumbnail -->
+											<div class="custom post-thumbnail">
+												<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
+											</div><!-- post-thumbnail -->
+											<p class="postExcerpt"><?php echo get_the_excerpt(); ?></p>
+										</div>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+				<?php
+				}
+				wp_reset_postdata();
+				?>
+			</aside>
+				<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();    
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+						get_template_part( 'template-parts/content/content', 'page' );
+					
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
+					endwhile; // End of the loop.
+				?>
+			</article>	
+			<div class='cell large-1'></div>
+			
 
 		</main><!-- #main -->
-	</div><!-- #primary -->
+	</section><!-- #primary -->
 
 <?php
-// get_sidebar();
 get_footer();
