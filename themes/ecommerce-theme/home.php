6bae90b80016d6c67b96fd1580b1c463ce499ff0
Karan Sandhu <karansmart943@gmail.com>
2020-03-09 21:04:07 -0400

    Got the block header in place in blog archive page.

--- themes/ecommerce-theme/home.php
+++ themes/ecommerce-theme/home.php
@@ -9,7 +9,7 @@ get_header();
 	<section id="primary" class="content-area grid-container">
 		<main id="main" class="site-main">
 			<div class=" cell large-1"></div>
-
+				<h1 class="blogTitle">BLOG</h1>
 			<article class="cell large-10" id="bodyText">
 				<?php
 				/**
