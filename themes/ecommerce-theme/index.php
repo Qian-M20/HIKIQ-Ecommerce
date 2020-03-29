bb3f7ba88b117f3057215b05d19cca6e26700a17
Karan Sandhu <karansmart943@gmail.com>
2020-03-29 00:43:02 -0400

    Script for chat in index.php

--- themes/ecommerce-theme/index.php
+++ themes/ecommerce-theme/index.php
@@ -50,10 +50,8 @@ get_header();
 
 		endif;
 		?>
-
-<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'beb0346f-01d5-4bb2-9b67-ef791541cbd4', f: true }); done = true; } }; })();</script>
-
 		</main><!-- #main -->
+		<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'beb0346f-01d5-4bb2-9b67-ef791541cbd4', f: true }); done = true; } }; })();</script>
 	</div><!-- #primary -->
 
 <?php
